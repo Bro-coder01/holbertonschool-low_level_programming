afeb68f5f40792b919eefc998c75a1fdba6db105
Bro-coder01 <ally185512@gmail.com>
2026-03-05 22:43:03 +0000

    Done

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,9 +2,9 @@
 
 /**
  * _atoi - converts a string to an integer
- * @s: string to be converted
+ * @s: the string to be converted
  *
- * Return: the integer value of the converted string
+ * Return: integer value of the converted string
  */
 int _atoi(char *s)
 {
@@ -18,7 +18,7 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			sign *= -1;
 
-		if (s[i] >= '0' && s[i] <= '10')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			found = 1;
 			res = (res * 10) + (s[i] - '0');
