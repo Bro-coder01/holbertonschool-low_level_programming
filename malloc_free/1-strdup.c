01357bca51ec3fc72eecb3e3c8018d3b1e4e10fa
Bro-coder01 <ally185512@gmail.com>
2026-04-08 15:36:59 +0000

    Deal with malloc

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -16,12 +16,12 @@ char *_strdup(char *str)
 		return (NULL);
 
 	for (i = 0; str[i] != '\0'; i++)
-	{
+		;
 
-	}
+	p = (char *)malloc(sizeof(char) * (i + 1));
 
-	p = malloc(sizeof(char) * (i + 1));
-
+	if (p == NULL)
+		return (NULL);
 
 	for (j = 0; j <= i; j++)
 	{
