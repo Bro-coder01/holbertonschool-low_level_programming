0783ecb3b5efb1a7b9bf3323caa4c26981b863ac
Bro-coder01 <ally185512@gmail.com>
2026-04-23 13:51:56 +0000

    Main files has been created

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,15 +1,13 @@
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- *
- * Description: singly linked list node structure
  */
 typedef struct list_s
 {
@@ -17,3 +15,11 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif /* LISTS_H */
