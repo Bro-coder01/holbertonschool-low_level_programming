f30af1bdfd4c64cf7b2e651faeb5b8d676ee589e
Bro-coder01 <ally185512@gmail.com>
2026-04-23 13:40:00 +0000

    Main files has been created

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the list_t list
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nodes;
+
+	for (nodes = 0; h; nodes++)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+
+		h = h->next;
+	}
+
+	return (nodes);
+}
