f6bdb94d43657b188090aa750e3d524b50d50c9e
Bro-coder01 <ally185512@gmail.com>
2026-04-25 20:28:08 +0000

    Done

--- file_io/1-main.c
+++ file_io/1-main.c
@@ -1,42 +1,22 @@
-#include <fcntl.h>
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * create_file - Creates a file with specific permissions and content.
- * @filename: Name of the file to create.
- * @text_content: NULL terminated string to write to the file.
+ * main - check the code
  *
- * Return: 1 on success, -1 on failure.
+ * Return: Always 0.
  */
-int create_file(const char *filename, char *text_content)
+int main(int ac, char **av)
 {
-	int fd, len = 0, res_write;
+    int res;
 
-	if (filename == NULL)
-		return (-1);
-
-	/* Open with: Create if missing, Write only, Truncate if exists */
-	/* Permissions: 0600 (rw-------) */
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-
-	if (text_content != NULL)
-	{
-		/* Calculate length of text_content */
-		while (text_content[len])
-			len++;
-
-		/* Write content to file */
-		res_write = write(fd, text_content, len);
-		if (res_write == -1 || res_write != len)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
-
-	close(fd);
-	return (1);
+    if (ac != 3)
+    {
+        dprintf(2, "Usage: %s filename text\n", av[0]);
+        exit(1);
+    }
+    res = create_file(av[1], av[2]);
+    printf("-> %i)\n", res);
+    return (0);
 }
