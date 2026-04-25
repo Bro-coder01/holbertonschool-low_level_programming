42c071f70b41ed6137f8a58246d9622ae9aae74a
Bro-coder01 <ally185512@gmail.com>
2026-04-25 20:19:33 +0000

    Done

--- file_io/1-main.c
+++ file_io/1-main.c
@@ -1,22 +1,42 @@
-#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * main - check the code
+ * create_file - Creates a file with specific permissions and content.
+ * @filename: Name of the file to create.
+ * @text_content: NULL terminated string to write to the file.
  *
- * Return: Always 0.
+ * Return: 1 on success, -1 on failure.
  */
-int main(int ac, char **av)
+int create_file(const char *filename, char *text_content)
 {
-    int res;
+	int fd, len = 0, res_write;
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = create_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+	if (filename == NULL)
+		return (-1);
+
+	/* Open with: Create if missing, Write only, Truncate if exists */
+	/* Permissions: 0600 (rw-------) */
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		/* Calculate length of text_content */
+		while (text_content[len])
+			len++;
+
+		/* Write content to file */
+		res_write = write(fd, text_content, len);
+		if (res_write == -1 || res_write != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
 }
